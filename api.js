2f613b90a7190c6cff2fadb4d13194c46ecd629a
bzamansky <batya.zamansky@gmail.com>
2015-10-08 13:59:13 -0400

    can get data from db, started using jade layout

--- api.js
+++ api.js
@@ -7,35 +7,51 @@ var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/playlists';
 
 function populate(db, callback){
-	for (var i = 0; i < 30; i++) {
+	for (var i = 0; i < 10; i++) {
 		insertPrompt(db, callback);
 	};
 }
 
 function insertPrompt(db, callback){
-	var prompt = getPrompt();
-	db.collection('promptsv1').insertOne({
-		"prompt" : prompt
-	}, function(err,result){
-		assert.equal(err,null);
-		console.log("Inserted a prompt");
-		callback(result);
-	})
+	getPromptAPI(function(prompt){
+		db.collection('promptsv1').insertOne({
+			"prompt" : prompt
+		}, function(err,result){
+			assert.equal(err,null);
+			console.log("Inserted a prompt");
+			callback(result);
+		})
+	});
+	
 }
 
-
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  populate(db, function(){
-  	db.close();
-  });
-});
-
-function getPrompt(){
+function getPromptAPI(callback){
 	var url = "http://ineedaprompt.com?api";
 	request.get(url, function(err, request,body){
-		return body;
+		//console.log(body);
+		callback(body);
 	});
 }
+
+function getPromptDB(db,callback){
+	var prompts = db.collection("promptsv1").find();
+	prompts.each(function(err,p){
+		assert.equal(err,null);
+		if (p != null){
+			console.dir(p.prompt);
+		}
+		else{
+			callback;
+		}
+	});
+}
+
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	//populate(db, function(){});
+	getPromptDB(db, function(){});
+});
+
+
 
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var path = require('path');
 var api = require('./api.js');
 
 var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.get('/', function(req,res){
-	res.send(api.getPrompts());
+	//res.send(api.getPrompts());
+	res.render('index',{title:'Hello World'});
 });
 
 var server = app.listen(3000, function () {
